be5822807d81d48681e1e8cdf6e4e02967e2c26b
Kyle Treman <jkt290@nyu.edu>
2018-02-19 14:51:18 -0500

    Add Icon C

---
+++ src/components/Icon.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FontIcon from 'material-ui/FontIcon'
+
+// this class takes the following props
+// # grid positioning
+// # colors.color
+// # text of material icon
+
+export default class NavPage extends React.Component {
+  render() {
+    return (<div style={this.props.grid}>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"></link>
+      <FontIcon className='material-icons'
+        style={this.props.colors.iconGreyStyle}>
+        {this.props.text}
+      </FontIcon>
+    </div>
+    )
+  }
+}
