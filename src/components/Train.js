cab1414a225059dd947a0a7d737755ce51e8e55d
Kyle Treman <jkt290@nyu.edu>
2018-02-19 14:53:20 -0500

    Add Train C

---
+++ src/components/Train.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Rectangle from './Rectangle'
+
+
+export default class Train extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // positioning to pass each rendered component
+      container: {
+        display: 'grid',
+        margin: '0 auto',
+        maxWidth: '70%'
+      },
+      question: {
+        gridRow: '1',
+        gridColumn: '1'
+      },
+      answer: {
+        gridRow: '1',
+        gridColumn: '2'
+      }
+    }
+  }
+  render() {
+    return (
+      <div style={this.state.container}>
+        <Rectangle grid={this.state.question} style={this.props.colors.white} text={'Question?'}/>
+        <Rectangle grid={this.state.answer} style={this.props.colors.blue} text={'Answer!'} />
+      </div>
+    )
+  }
+}
+
+// before Rectangle component
+{/* <div style={Object.assign(this.props.colors.white, this.props.shape.rectangle)} className='question'><p>Question?</p></div> */}
+{/* <div style={Object.assign(this.props.colors.blue, this.props.shape.rectangle)} className='answer'><p>Answer!</p></div> */}
