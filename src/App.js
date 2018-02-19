18a39acd3606c2423fb68d51f9dbe26ec570544c
Kyle Treman <jkt290@nyu.edu>
2018-02-19 14:53:50 -0500

    Add App C

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// import Start from './components/js/Start'
+import NavPage from './components/js/NavPage'
+// import Train from './components/js/Train'
+// import Test from './components/js/Test'
+// import Create from './components/js/Create'
+// import AnimationTest from './components/js/AnimationsTest'
+
+const colors = {
+  pink: {
+    backgroundColor: 'rgb(252, 152, 218)',
+    border: '5px solid white',
+    color: 'white',
+  },
+  teal: {
+    backgroundColor: 'rgb(125, 188, 241)',
+    border: '5px solid white',
+    color: 'white',
+  },
+  green: {
+    backgroundColor: 'rgb(131, 201, 228)',
+    border: '5px solid white',
+    color: 'white',
+  },
+  blue: {
+    backgroundColor: 'rgb(120, 141, 214)',
+    border: '5px solid white',
+    color: 'white',
+  },
+  white: {
+    backgroundColor: 'white',
+    border: '5px solid black',
+    color: 'black',
+  },
+  iconGreyStyle: {
+    color: 'rgb(105, 105, 105)',
+    fontSize: '36px'
+  },
+  iconWhiteStyle: {
+    color: 'white'
+  }
+}
+const spacing = {
+  paddingTop: '10%'
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.combineStyleObjects = this.combineStyleObjects.bind(this)
+  }
+  // combines multiple 1 level objects for inline style
+  combineStyleObjects(...params) {
+    var resultObject = params.reduce((result, currentObject) => {
+      for(var key in currentObject) {
+          if (currentObject.hasOwnProperty(key)) {
+              result[key] = currentObject[key];
+          }
+      }
+      return result;
+    }, {});
+    return resultObject
+  }
+  /*
+  Each Page takes
+  # combineStyleObjects function
+  # full colors obj -L2 with matching borders + Icon styling
+  */
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      // <MuiThemeProvider>
+        <div style={spacing} className="App">
+          {/* <Start colors={colors}/> */}
+        <NavPage colors={colors}
+        combineStyleObjects={this.combineStyleObjects}/>
+        {/* <AnimationTest
+          combineStyleObjects={this.combineStyleObjects}
+          colors={colors} /> */}
+          {/* <Create colors={colors} /> */}
+          {/* <Train colors={colors} /> */}
+          {/* <Test colors={colors} /> */}
+        </div>
+      // </MuiThemeProvider>
+    )
   }
 }
 
