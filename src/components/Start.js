97d3e8e6a2af3b94d93e37ac22b6702d2f92742c
Kyle Treman <jkt290@nyu.edu>
2018-02-19 14:52:37 -0500

    Add Start C

---
+++ src/components/Start.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/Start.css'
+
+export default class Start extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className='title'>
+          Welcome to
+        </h2>
+        <span className='titleApp'>
+          Flash
+        </span>
+      </div>
+    )
+  }
+}
