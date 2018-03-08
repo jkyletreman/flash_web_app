80a2aeb59bc3a815c37182734955ea5bbf13f744
Kyle Treman <jkt290@nyu.edu>
2018-03-08 10:34:23 -0500

    Clean code

--- src/components/js/Icon.js
+++ src/components/js/Icon.js
@@ -8,12 +8,17 @@ import FontIcon from 'material-ui/FontIcon'
 
 export default class NavPage extends React.Component {
   render() {
-    return (<div style={this.props.grid}>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"></link>
-      <FontIcon className='material-icons'
-        style={this.props.colors.iconGreyStyle} onClick={this.props.onClick}>
-        {this.props.text}
-      </FontIcon>
+    return (
+      <div style={this.props.grid}>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"></link>
+
+        <FontIcon
+          className='material-icons'
+          style={this.props.colors.iconGreyStyle}
+          onClick={this.props.onClick}>
+          {this.props.text}
+        </FontIcon>
+
     </div>
     )
   }
